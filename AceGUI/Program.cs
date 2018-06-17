5158191ecfc8b02ee4e35e046189822e20207ea2
Bill Hertzing <bill.hertzing@gmail.com>
2018-06-17 07:54:22 -0400

    Compiles and runs dropdown component for enumerations with custom attributes not working Added Attribution.md minor changes to editorconfig and gitignore formatting improvements

--- AceGUI/Program.cs
+++ AceGUI/Program.cs
@@ -13,11 +13,11 @@ namespace AceGUI
         {
             var serviceProvider = new BrowserServiceProvider(services =>
             {
-                // Add Blazor.Extensions.Logging.BrowserConsoleLogger; taken from the Blazor.Extensions.Logging NuGet package home page https://www.nuget.org/packages/Blazor.Extensions.Logging/# on 6/12/2018
-                services.AddLogging(builder => builder
-                    .AddBrowserConsole() // Register the logger with the ILoggerBuilder
-                    .SetMinimumLevel(LogLevel.Debug) // Set the minimum log level to Information
-                );
+              // Add Blazor.Extensions.Logging.BrowserConsoleLogger; taken from the Blazor.Extensions.Logging NuGet package home page https://www.nuget.org/packages/Blazor.Extensions.Logging/# on 6/12/2018
+              services.AddLogging(builder => builder
+                  .AddBrowserConsole() // Register the logger with the ILoggerBuilder
+                  .SetMinimumLevel(LogLevel.Debug) // Set the minimum log level to Information
+              );
                 // Add any additional custom services here
             });
 
