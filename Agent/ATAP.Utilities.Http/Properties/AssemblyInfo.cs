5cf542f7d7a5869a0f39b3252a1e533414707e2c
Bill Hertzing <bill.hertzing@gmail.com>
2018-11-18 20:32:56 -0500

    another incorect update to all BuildNumbers

--- Agent/ATAP.Utilities.Http/Properties/AssemblyInfo.cs
+++ Agent/ATAP.Utilities.Http/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 // ATAP.Utilities.BuildTooling.targets will update the build (date), and revision fields each time a new build occurs
-[assembly:AssemblyFileVersion("1.0.6894.28436")]
+[assembly:AssemblyFileVersion("1.0.6897.2732")]
 // ATAP.Utilities.BuildTooling.targets will update the AssemblyInformationalVersion field each time a new build occurs
 [assembly:AssemblyInformationalVersion("1.0.0")]
 [assembly:AssemblyVersion("1.0.0")]
