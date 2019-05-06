cece397d4bda5cbd8df16626e849debe57ecb805
William Hertzing <bill.hertzing@gmail.com>
2019-05-05 21:31:58 -0600

    starting conversion, just before major filename replacement

--- Agent/Service/Program.cs
+++ Agent/Service/Program.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading;
-
+using System.Threading.Tasks;
 using ServiceStack.Logging;
 using ServiceStack.Logging.NLogger;
-using Topshelf;
 
 namespace Ace.AceService {
     partial class Program {
@@ -21,16 +21,37 @@ namespace Ace.AceService {
 
         public static ILog Log;
 
-        static void Main(string[] args) {
+        public static async Task Main(string[] args) {
             //To ensure every ServiceStack service uses the same Global Logger, set it before you initialize ServiceStack's AppHost,
             LogManager.LogFactory = new NLogFactory();
             Log = LogManager.GetLogger(typeof(Program));
             Log.Debug("Entering Program.Main");
+
             // When running as a service, the initial working dir is usually %WinDir%\System32, but the program (and configuration files) is probably installed to a different directory
             // Change the working dir to the location where the Exe and configuration files are installed to.
             var loadedFromDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
             Log.Debug($"loadedFromDir is {loadedFromDir}");
             System.IO.Directory.SetCurrentDirectory(loadedFromDir);
+
+            // Create the web server host
+            // Create a self-hosted host with just Kestrel
+            Log.Debug("in Program.Main: create webHostBuilderSelfHostedKestrel");
+
+            var webHostBuilderSelfHostedKestrel = new WebHostBuilder()
+                .UseKestrel()
+                // In V30P4, all SS interfaces return an error that "synchronous writes are disallowed", see following issue
+                //  https://github.com/aspnet/AspNetCore/issues/8302
+                // Woraround is to configure the default web server to AllowSynchronousIO=true
+                // ToDo: see if this is fixed in a release after V30P4
+                .ConfigureKestrel((context, options) => {
+                    options.AllowSynchronousIO=true;
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS")??"http://localhost:21200/")
+                ;
+
+            var webHostBuilder = webHostBuilderSelfHostedKestrel;
             // setup shutdown handlers
             // process machine resources
             // confirm parameters
