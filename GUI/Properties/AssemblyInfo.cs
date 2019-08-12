bf19b6c9aea06cc48635ab1ca5d8046f56439935
Bill Hertzing <bill.hertzing@gmail.com>
2019-08-12 09:11:14 -0600

    Squashed commit of the following:
    commit ed5d1db25d8d5ac2b3a4a2ea215ade352b4f47d4
    Author: Bill Hertzing <bill.hertzing@gmail.com>
    Date:   Mon Aug 12 09:09:19 2019 -0600
    
        Upgrade complete, compiles and runs
    
    commit 7f2393e0a1208636533cabe3e20b379432c82534
    Author: Bill Hertzing <bill.hertzing@gmail.com>
    Date:   Mon Aug 12 09:08:28 2019 -0600
    
        Update complete, compiles and runs

--- GUI/Properties/AssemblyInfo.cs
+++ GUI/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 using System.Reflection;
 
 // ATAP.Utilities.BuildTooling.targets will update the build (days since 1/1/2000), and revision (seconds since midnight, UTC, / 2) fields each time a new build occurs
-[assembly:AssemblyFileVersion("1.1.7157.7954")]
+[assembly:AssemblyFileVersion("1.1.7163.27179")]
 // ATAP.Utilities.BuildTooling.targets will update the AssemblyInformationalVersion field each time a new build occurs
-[assembly:AssemblyInformationalVersion("1.1.0-Alpha-013")]
+[assembly:AssemblyInformationalVersion("1.1.0-Alpha-015")]
 [assembly:AssemblyVersion("1.1.0")]
